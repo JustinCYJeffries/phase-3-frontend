c9484233aebe4d9093209e1cfca862a769b8488e
Justin Jeffries <justincyjeffries@gmail.com>
2022-08-29 01:12:27 -0400

    formats app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react"
+import PortfolioContainer from './PortfolioContainer'
+import CryptoContainer from './CryptoContainer'
 
 function App() {
+  const [portfolioList, setPortfolioList] = useState([])
+  const [cryptoList, setCryptoList] = useState([])
+  const [purchaseList, setPurchaseList] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/portfolios")
+      .then((r) => r.json())
+      .then(setPortfolioList);
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:9292/cryptos")
+      .then((r) => r.json())
+      .then(setCryptoList);
+  }, []);
+  
+  useEffect(() => {
+    fetch("http://localhost:9292/purchases")
+      .then((r) => r.json())
+      .then(setPurchaseList);
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="header">
+     <PortfolioContainer portfolioList={portfolioList}/>
+     </div>
+     <table>
+      <tr>
+        <td width="30%">
+        <CryptoContainer cryptoList={cryptoList} />
+        </td>
+        <td>
+          <InfoContainer cryptoList={cryptoList} portfolioList={portfolioList} purchaseList={purchaseList}/>
+        </td>
+      </tr>
+     </table>
+
+
     </div>
   );
 }
